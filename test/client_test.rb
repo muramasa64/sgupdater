307e2c5097c56dbfeaa1eef42ac850b6b596738c
ISOBE Kazuhiko <muramasa64@gmail.com>
2015-07-30 21:38:30 +0900

    fix test

--- test/client_test.rb
+++ test/client_test.rb
@@ -43,6 +43,7 @@ class TestClient < Test::Unit::TestCase
       assert_equal(@@config['vpc_id'], sg.vpc_id)
 
       fx = @@fixture['security_groups'].find {|f| f['group_name'] == sg.group_name}
+      next if fx
       assert_equal(fx['group_name'], sg.group_name)
       assert_equal(fx['description'], sg.description)
 
