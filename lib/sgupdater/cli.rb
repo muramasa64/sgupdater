108459d6dd4d8c8ea068329b9bcab8b49425f566
ISOBE Kazuhiko <muramasa64@gmail.com>
2015-07-30 21:37:09 +0900

    add show option 'to_cidr'

--- lib/sgupdater/cli.rb
+++ lib/sgupdater/cli.rb
@@ -9,8 +9,9 @@ module Sgupdater
 
     desc :show, "Show current permissions"
     method_option :from_cidr, type: :string, required: true
+    method_option :to_cidr, type: :string, required: false
     def show
-      client.get.each {|sg| show_security_groups(sg, options[:from_cidr])}
+      client.get.each {|sg| show_security_groups(sg, options[:from_cidr], options[:to_cidr])}
     end
 
     desc :update, "Update cidr address"
@@ -30,11 +31,25 @@ module Sgupdater
       @client ||= Client.new options, aws_configuration
     end
 
-    def show_security_groups(sg, cidr)
+    def cidr_in_ip_permission?(ip_permission, cidr)
+      ip_permission.ip_ranges.select {|ip| ip.values.include? cidr}.size > 0
+    end
+
+    def cidr1_in_ip_permission_and_cidr2_not_in_ip_permission?(ip_permission, cidr1, cidr2)
+       cidr1_find = cidr_in_ip_permission?(ip_permission, cidr1)
+       cidr2_not_find = !cidr_in_ip_permission?(ip_permission, cidr2)
+       cidr1_find && cidr2_not_find
+    end
+
+    def show_security_groups(sg, from_cidr, to_cidr)
       sg.ip_permissions.each do |perm|
-        perm.ip_ranges.select {|ip| ip.values.include? cidr}.each do
-          puts [sg.vpc_id || '(classic)', sg.group_id, sg.group_name, perm.from_port, perm.to_port, cidr].join("\t")
+        found = false
+        if to_cidr
+          found = cidr1_in_ip_permission_and_cidr2_not_in_ip_permission?(perm, from_cidr, to_cidr)
+        else
+          found = cidr_in_ip_permission?(perm, from_cidr)
         end
+        puts [sg.vpc_id || '(classic)', sg.group_id, sg.group_name, perm.from_port, perm.to_port, from_cidr].join("\t") if found
       end
     end
   end
